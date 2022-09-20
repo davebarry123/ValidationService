ef5064d5bc341c2bd73c3f844a91dac1b1342b79
davebarry123 <dachalis@microsoft.com>
2022-09-20 16:43:36 -0700

    updated files

---
+++ Classes/GrammarResponse.cs
@@ -0,0 +1,94 @@
+﻿namespace ValidationService.Classes
+{
+
+    public class GrammarResponse
+    {
+        public Software software { get; set; }
+        public Warnings warnings { get; set; }
+        public Language language { get; set; }
+        public Match[] matches { get; set; }
+        public int[][] sentenceRanges { get; set; }
+    }
+
+    public class Software
+    {
+        public string name { get; set; }
+        public string version { get; set; }
+        public string buildDate { get; set; }
+        public int apiVersion { get; set; }
+        public bool premium { get; set; }
+        public string premiumHint { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Warnings
+    {
+        public bool incompleteResults { get; set; }
+    }
+
+    public class Language
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public Detectedlanguage detectedLanguage { get; set; }
+    }
+
+    public class Detectedlanguage
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public float confidence { get; set; }
+        public string source { get; set; }
+    }
+
+    public class Match
+    {
+        public string message { get; set; }
+        public string shortMessage { get; set; }
+        public Replacement[] replacements { get; set; }
+        public int offset { get; set; }
+        public int length { get; set; }
+        public Context context { get; set; }
+        public string sentence { get; set; }
+        public Type type { get; set; }
+        public Rule rule { get; set; }
+        public bool ignoreForIncompleteSentence { get; set; }
+        public int contextForSureMatch { get; set; }
+    }
+
+    public class Context
+    {
+        public string text { get; set; }
+        public int offset { get; set; }
+        public int length { get; set; }
+    }
+
+    public class Type
+    {
+        public string typeName { get; set; }
+    }
+
+    public class Rule
+    {
+        public string id { get; set; }
+        public string subId { get; set; }
+        public string sourceFile { get; set; }
+        public string description { get; set; }
+        public string issueType { get; set; }
+        public Category category { get; set; }
+        public bool isPremium { get; set; }
+    }
+
+    public class Category
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Replacement
+    {
+        public string value { get; set; }
+        public string shortDescription { get; set; }
+    }
+
+}
--- Classes/GrammarValidationRule.cs
+++ Classes/GrammarValidationRule.cs
@@ -1,33 +1,65 @@
 ﻿namespace ValidationService.Classes
 {
-    using Microsoft.Office.Interop.Word;
+    using Newtonsoft.Json;
     using System.Reflection;
 
     public class GrammarValidationRule : BaseValidationRule
     {
         public GrammarValidationRule(string body)
         {
-            this.Name = Assembly.GetExecutingAssembly().GetName().Name; 
+            this.Name = Assembly.GetExecutingAssembly()?.GetName()?.Name; 
             this.Body = body;
         }
 
-        public override List<ValidationResult> Validate()
+        public override async Task<List<ValidationResult>> Validate()
         {
             var result = new List<ValidationResult>();
-            Application myWord = new Application();
-            
-            if (!myWord.CheckGrammar(this.Body))
+            string[] lines = this.Body.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            for (int lineNum = 1; lineNum < lines.Length + 1; lineNum++)
             {
-                result.Add(
-                    new ValidationResult 
-                    { 
-                        LineNumber = 1, 
-                        Message = $"Invalid grammar {this.Body}", 
-                        Severity = ValidationSeverity.Warning 
-                    });
+                result.AddRange(await this.CheckGrammar(lineNum, lines[lineNum - 1]));
             }
 
             return result;
+        }
+
+        private async Task<List<ValidationResult>> CheckGrammar(int lineNum, string line)
+        {
+            var output = new List<ValidationResult>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://api.languagetool.org/v2/check"),
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        { "text", line },
+                        { "language", "auto" },
+                    }),
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var op = JsonConvert.DeserializeObject<GrammarResponse>(body);
+                if (op?.matches?.Length > 0)
+                {
+                    foreach (var match in op.matches)
+                    {
+                        output.Add(
+                            new ValidationResult
+                            {
+                                LineNumber = lineNum,
+                                Message = match.message,
+                                Severity = ValidationSeverity.Error
+                            });
+                    }
+                }
+            }
+
+            return output;
         }
     }
 }
